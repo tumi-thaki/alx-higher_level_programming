c08d3080a89f918ec0af59fa2c54989834fdd7b0
Tumi Thaki <tumiethaki01@gmail.com>
2023-10-31 08:42:53 -0700

    all tasks upload

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Tumisang Thaki
+
+def pow(a, b):
+    """Return a to the power of b."""
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Tumisang Thaki
+
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
