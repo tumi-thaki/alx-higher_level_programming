18e9311b7eb25a5c7ade7bf7cf096b77586aa29b
Tumi Thaki <tumiethaki01@gmail.com>
2023-11-08 18:08:28 -0800

    0-square_matrix_simple.py update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 # Author  Tumi Thaki
 
 def square_matrix_simple(matrix=[]):
-        """Compute the square value of all integers of a matrix."""
-            return ([list(map(lambda x: x * x, row)) for row in matrix])
+    """Compute the square value of all integers of a matrix."""
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
