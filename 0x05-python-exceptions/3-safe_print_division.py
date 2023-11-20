a89a351fc537f161936d752a59ceaf9f92e348c6
Tumi Thaki <tumiethaki01@gmail.com>
2023-11-20 12:03:50 -0800

    all taks done

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Author - Tumi Thaki
+
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Tumi Thaki
+
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
