786a8809473c64645e34b19d9f54285c3c2f64ac
Tumi Thaki <tumiethaki01@gmail.com>
2023-12-05 15:22:37 -0800

    all tasks upload

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author  Tumi Thaki
+
+"""Defines a string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - Tumi Thak
+i
+"""Defines a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict___
