a5722c86e0cc01eea0169b4197f6f483e238b639
Tumi Thaki <tumiethaki01@gmail.com>
2024-02-13 09:54:39 -0800

    2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+* Check the parameters provided
+*/
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+};
