1685c7ca44603a059b72bea10f81d1da4ee8aaa9
Tumi Thaki <tumiethaki01@gmail.com>
2024-03-01 17:53:21 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Sends a request to the URL and displays the value of the variable X-Request-Id
-in the response header"""
+Python script that fetches an URL with requests package
+"""
+import requests
 
-if __name__ == '__main__':
-    from requests import get
-    from sys import argv
 
-    res = get(argv[1])
-    print(res.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+        r = requests.get('https://alx-intranet.hbtn.io/status')
+            t = r.text
+                print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
